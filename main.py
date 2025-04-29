bef4ace0c6c90e7317dc21e416da7f6598c0568c
Adel M. Abdallah, PhD <adel.m.abdallah@gmail.com>
2025-04-29 15:16:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -446,4 +446,4 @@ def index():
 # --- Main Execution Block ---
 if __name__ == "__main__":
     is_debug = os.environ.get("FLASK_ENV") == "development"
-    app.run(host="0.0.0.0", port=5000, debug=is_debug, use_reloader=is_debug)
+    app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
